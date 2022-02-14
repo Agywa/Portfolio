3d0692910606ba59944e83b223b6446c8f6c6567
Agywa <91954956+Agywa@users.noreply.github.com>
2022-02-14 20:38:39 +0300

    111

---
+++ src/skills/Skills.tsx
@@ -0,0 +1,21 @@
+import React from 'react';
+import style from "./Skills.module.css"
+import styleContainer from "../common/styles/Container.module.css";
+
+const Skills = () => {
+    return (
+        <div className={style.skillsBlock}>
+            <div className= {`${styleContainer.container} ${style.skillsContainer}`}>
+               <h2 className={style.title}>Skills</h2>
+                <div className={style.skills}>
+
+<h2>asfasfasf</h2>
+
+
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default Skills
